4d5e31c3ffa822066612d40fb7fa3268fa08b37a
Sleeepp <149921328+Sleeepp@users.noreply.github.com>
2024-03-07 19:38:25 +0700

    Update fave.js

--- fave.js
+++ fave.js
@@ -1,21 +1,21 @@
-    var fave = fave || {};
-    var data = JSON.parse(localStorage.getItem("favorite"));
-    data = data || {};
-            var fave = fave || {};
-            var data = JSON.parse(localStorage.getItem("favorite"));
-            data = data || {};
-            $(function () {
-                var data = localStorage.getItem("favorite");
+var fave = fave || {};
+var data = JSON.parse(localStorage.getItem("favorite"));
+data = data || {};
+        var fave = fave || {};
+        var data = JSON.parse(localStorage.getItem("favorite"));
+        data = data || {};
+        $(function () {
+            var data = localStorage.getItem("favorite");
 
-                if (data !== null) {
-                    $("input[name='favorites']").attr("checked", "checked");
-                }
-            });
-            $("input[name='favorites']").click(function () {
-                if ($(this).is(":checked")) {
-                localStorage.setItem("favorite", $(this).val());
-                } 
-                else {
-                    localStorage.removeItem("favorite");
-                }
-            });
+            if (data !== null) {
+                $("input[name='favorites']").attr("checked", "checked");
+            }
+        });
+        $("input[name='favorites']").click(function () {
+            if ($(this).is(":checked")) {
+            localStorage.setItem("favorite", $(this).val());
+            } 
+            else {
+                localStorage.removeItem("favorite");
+            }
+        });
